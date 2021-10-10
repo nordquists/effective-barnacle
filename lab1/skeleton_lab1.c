221c56e6df70002f8a38fb1a9d1610d8fa784f2e
nordquists <srn334@nyu.edu>
2021-10-10 12:23:38 -0400

    d

--- lab1/skeleton_lab1.c
+++ lab1/skeleton_lab1.c
@@ -107,7 +107,7 @@ int main(int argc, char *argv[]){
 
     //Write the numbers divisible by x in the file as indicated in the lab description.
 
-    for(i=0;i<=received[0] + receieved[1];i++){ 
+    for(i=0;i<=received[0] + received[1];i++){ 
       fprintf(fp, "%d \n", local_array[i]); 
     } 
 

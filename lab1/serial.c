b6a62ed19b6291436f44bcdcd886b69f9747bc61
nordquists <srn334@nyu.edu>
2021-10-12 11:37:56 -0400

    d

--- lab1/serial.c
+++ lab1/serial.c
@@ -141,13 +141,13 @@ end_p2 = clock();
 start_p3 = clock();
 
 if (rank == 0) {
-    results = (int *)malloc( (split + 1) * size * sizeof(int) );
+    results = (int *)malloc( n * sizeof(int) );
     int i;
     for ( i = 0 ; i < n ; i++ ) {
         results[i] = -1 ;
     }
   
-    MPI_Gather(local_array, curr, MPI_INT, results, (split + 1) * size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(local_array, curr, MPI_INT, results, n, MPI_INT, 0, MPI_COMM_WORLD);
 
     strcpy(filename, argv[1]);
     strcat(filename, ".txt");
@@ -167,7 +167,7 @@ if (rank == 0) {
 
     fclose(fp);
   } else {
-    MPI_Gather(local_array, curr, MPI_INT, results, (split + 1) * size, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(local_array, curr, MPI_INT, results, n, MPI_INT, 0, MPI_COMM_WORLD);
   }
 
 end_p3 = clock();

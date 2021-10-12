9617815091c4b62a05fcf2c3f1e8f7e44ee02044
nordquists <srn334@nyu.edu>
2021-10-12 11:44:04 -0400

    d

--- lab1/serial.c
+++ lab1/serial.c
@@ -158,7 +158,7 @@ if (rank == 0) {
       exit(1);
     }
 
-    for(i=0;i<=n;i++){ 
+    for(i=0;i< n;i++){ 
       if (results[i] != -1) {
         fprintf(fp, "%d \n", results[i]); 
         printf("result %d\n", results[i]);

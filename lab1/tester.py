d6922f1c69b30c4919db6c094959141d15615f67
nordquists <srn334@nyu.edu>
2021-10-14 19:39:48 -0400

    d

--- lab1/tester.py
+++ lab1/tester.py
@@ -19,7 +19,7 @@ def compare(output_file, N, x):
                 return False
             curr += 1
         if curr != len(results) - 1:
-            print(results)
+            print(curr, len(results) - 1)
             return False
     return True
 

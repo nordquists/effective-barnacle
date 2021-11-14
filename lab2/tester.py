81baded9cefb474d7da626aa60f1eb1a98577369
nordquists <srn334@nyu.edu>
2021-11-14 13:43:25 -0500

    d

--- lab2/tester.py
+++ lab2/tester.py
@@ -3,9 +3,8 @@ import math
 
 def generate_bins(nums, num_bins):
     bin_counts = [0 for bin in range(num_bins)]
-    for data_point in nums:
-        bin_number = data_point // (20 / num_bins)
-        print(bin_number)
+    for i in range(1, len(nums)):
+        bin_number = nums[i] // (20 / num_bins)
         bin_counts[bin_number] += 1
     
     return bin_counts

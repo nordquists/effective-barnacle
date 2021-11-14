c2e3f47ecacbcd84163114e51500be156f04f7ed
nordquists <srn334@nyu.edu>
2021-11-14 13:11:44 -0500

    d

--- lab2/tester.py
+++ lab2/tester.py
@@ -15,8 +15,8 @@ def compare(num_file, num_bins):
             nums.extend([int(item) for item in line.split()])
     results = generate_bins(nums, num_bins)
     
-    for i, bin in enumerate(results):
-        print(f'bin[{i}] = {bin}')
+    for i, b in enumerate(results):
+        print(f"bin[{i}] = {b}")
 
 if __name__ == '__main__':
     if compare(sys.argv[1], int(sys.argv[3])):

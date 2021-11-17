175ad65a49809b81b49a8978f0b910ac13280467
nordquists <srn334@nyu.edu>
2021-11-17 14:06:14 -0500

    d

--- lab2/srn334.c
+++ lab2/srn334.c
@@ -58,38 +58,38 @@ int main(int argc, char *argv[]) {
     start_parallel = clock();
 
    
-    // #pragma omp parallel for num_threads(threads) reduction(+:histogram)
-    // for(i = 0; i < num_nums; i++) {
-    //     // We want to map our numbers from [0, 20] -> [0, num_bins]
-    //     // if(nums[i] == 20.0) printf("Exact 20.0 found. \n");
-    //     histogram[(int)(nums[i] * scaled_bins)]++;
-    // }
-
-    #pragma omp parallel num_threads(threads)
-    {
-        int local_histogram[threads][num_bins];
-        int tid = omp_get_thread_num(); 
-
-        #pragma omp for 
-        for(i = 0; i < num_bins; i++) {
-            for(t = 0; t < threads; t++) {
-                local_histogram[t][i] = 0;
-            }
-        }
-
-        #pragma omp for 
-        for(i = 0; i < num_nums; i++) {
-            local_histogram[tid][(int)(nums[i] * scaled_bins)]++;
-        }
-
-        #pragma omp for
-        for(i = 0; i < num_bins; i++) {
-            for(t = 0; t < threads; t++) {
-                #pragma omp atomic
-                histogram[i] += local_histogram[t][i];
-            }
-        }
+    #pragma omp parallel for num_threads(threads) reduction(+:histogram)
+    for(i = 0; i < num_nums; i++) {
+        // We want to map our numbers from [0, 20] -> [0, num_bins]
+        // if(nums[i] == 20.0) printf("Exact 20.0 found. \n");
+        histogram[(int)(nums[i] * scaled_bins)]++;
     }
+
+    // #pragma omp parallel num_threads(threads)
+    // {
+    //     int local_histogram[threads][num_bins];
+    //     int tid = omp_get_thread_num(); 
+
+    //     #pragma omp for 
+    //     for(i = 0; i < num_bins; i++) {
+    //         for(t = 0; t < threads; t++) {
+    //             local_histogram[t][i] = 0;
+    //         }
+    //     }
+
+    //     #pragma omp for 
+    //     for(i = 0; i < num_nums; i++) {
+    //         local_histogram[tid][(int)(nums[i] * scaled_bins)]++;
+    //     }
+
+    //     #pragma omp for
+    //     for(i = 0; i < num_bins; i++) {
+    //         for(t = 0; t < threads; t++) {
+    //             #pragma omp atomic
+    //             histogram[i] += local_histogram[t][i];
+    //         }
+    //     }
+    // }
 
 
 

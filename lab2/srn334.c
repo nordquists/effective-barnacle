eee1ef2631328eed6410344b363f5a25e2c09e86
nordquists <srn334@nyu.edu>
2021-11-17 13:31:45 -0500

    d

--- lab2/srn334.c
+++ lab2/srn334.c
@@ -80,7 +80,7 @@ int main(int argc, char *argv[]) {
     //     histogram[(int)(nums[i] * scaled_bins)]++;
     // }
 
-    #pragma omp parallel for num_threads(threads)
+    #pragma omp parallel for num_threads(threads) reduction(+:histogram)
     for(i = 0; i < num_nums; i++) {
         // We want to map our numbers from [0, 20] -> [0, num_bins]
         // if(nums[i] == 20.0) printf("Exact 20.0 found. \n");

b552fcef9d6f9ea59d18975936c22e27727d167f
nordquists <srn334@nyu.edu>
2021-11-17 14:11:50 -0500

    d

--- lab2/srn334.c
+++ lab2/srn334.c
@@ -58,7 +58,7 @@ int main(int argc, char *argv[]) {
     start_parallel = clock();
 
    
-    #pragma omp parallel for num_threads(threads) reduction(+:histogram) default(none) private(j) shared(nums, scaled_bins, num_nums)
+    #pragma omp parallel for num_threads(threads) reduction(+:histogram) default(none) private(i) shared(nums, scaled_bins, num_nums)
     for(i = 0; i < num_nums; i++) {
         // We want to map our numbers from [0, 20] -> [0, num_bins]
         // if(nums[i] == 20.0) printf("Exact 20.0 found. \n");

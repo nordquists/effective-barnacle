42fa38307cda901aa6dc2232fcbd5bf6fc168288
nordquists <srn334@nyu.edu>
2021-12-01 11:43:00 -0500

    d

--- lab3/vectors.cu
+++ lab3/vectors.cu
@@ -135,7 +135,7 @@ __global__ void vecGPU(float* ad, float* bd, float* cd, int width) {
 			additional_work = width - (BLOCKS_PER_GRID * THREADS_PER_BLOCK * calcs_per_thead);
 
 			if(threadIdx.x < additional_work) {
-				cd[index + THREADS_PER_BLOCK + threadIdx.x] += ad[index + calcs_per_thead + threadIdx.x] * bd[index + THREADS_PER_BLOCK + threadIdx.x];
+				cd[index + THREADS_PER_BLOCK + threadIdx.x] += ad[index + THREADS_PER_BLOCK + threadIdx.x] * bd[index + THREADS_PER_BLOCK + threadIdx.x];
 			}
 		}
 	}

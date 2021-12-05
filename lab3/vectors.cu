1ee5d647962c4cc3c27d5361d86be882e606bda7
nordquists <srn334@nyu.edu>
2021-12-05 13:43:01 -0500

    d

--- lab3/vectors.cu
+++ lab3/vectors.cu
@@ -5,7 +5,7 @@
 
 #define RANGE 11.79
 
-#define BLOCKS_PER_GRID 8
+#define BLOCKS_PER_GRID 16
 #define THREADS_PER_BLOCK 500
 
 /*** TODO: insert the declaration of the kernel function below this line ***/

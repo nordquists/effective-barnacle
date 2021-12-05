8a017925a70a979e0458c1b09fce87d058f8cd76
nordquists <srn334@nyu.edu>
2021-12-05 11:40:43 -0500

    d

--- lab3/vectors.cu
+++ lab3/vectors.cu
@@ -113,10 +113,10 @@ int main(int argc, char *argv[]){
 	
 	//checking the correctness of the GPU part
 	for(i = 0; i < n; i++)
-	  if(temp[i] - c[i] > 0.001 || temp[i] - c[i] < -0.001 )
-		printf("Element %d in the result array does not match the sequential version (%lf vs. %lf)\n", i, c[i], temp[i]);
-	//   if(temp[i] != c[i])
+	//   if(temp[i] - c[i] > 0.001 || temp[i] - c[i] < -0.001 )
 	// 	printf("Element %d in the result array does not match the sequential version (%lf vs. %lf)\n", i, c[i], temp[i]);
+	  if(temp[i] != c[i])
+		printf("Element %d in the result array does not match the sequential version (%lf vs. %lf)\n", i, c[i], temp[i]);
 		
 	// Free the arrays in the host
 	free(a); free(b); free(c); free(temp);
